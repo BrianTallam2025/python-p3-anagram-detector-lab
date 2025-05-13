9efcaa5c20378af65040a1419f8598ae904fdffc
Brian Tallam <tallambrian633@gmail.com>
2025-05-13 21:13:29 +0300

    anandsj

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        original_sorted = sorted(self.word)
+        return [w for w in word_list if sorted(w) == original_sorted and w.lower() != self.word.lower()]
+
